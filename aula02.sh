1b8e6a3ac96fcbef53f7bf9fb2fda9c92b6abb4f
Michael Kevin <michaelkmb@hotmail.com>
2023-03-03 20:23:11 -0300

    example code video_02;

---
+++ aula02.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo $TERM
+echo $HOME
+echo $UID
+
+printenv | grep LANG
+
+#variáveis
+string='hello world'
+echo $string
+export string
+
+echo 'changing to bash to show export'
+bash | echo $string | exit
+echo 'back to '$SHELL
+
+unset string
