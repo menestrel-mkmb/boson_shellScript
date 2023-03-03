beb6c3b631d0dd64e07be759e274ad9f0ebb2f3b
Michael Kevin <michaelkmb@hotmail.com>
2023-03-03 20:37:39 -0300

    improvement video_02;

--- aula02.sh
+++ aula02.sh
@@ -12,7 +12,9 @@ echo $string
 export string
 
 echo 'changing to bash to show export'
-bash | echo $string | exit
+bash | echo $string || exit
 echo 'back to '$SHELL
 
 unset string
+echo 'removed string variable'
+echo $string
