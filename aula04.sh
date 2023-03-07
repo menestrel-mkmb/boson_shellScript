7dc7da90e95d457b4e696538bea43ae6bd6180a7
Michael Kevin <michaelkmb@hotmail.com>
2023-03-07 12:33:08 -0300

    example code video_04;

---
+++ aula04.sh
@@ -0,0 +1,16 @@
+alias mo='more'
+alias ll='ls -l'
+
+alias up='sudo pacman -Syyu'
+
+unalias mo
+echo 'alias nao funfa no terminal interno do vscode'
+
+echo '------------'
+addLine(){
+    cd ./testFolder/
+    echo 'linha adicionada' >> teste_aula04 
+}
+
+typeset -f | grep addLine
+addLine
