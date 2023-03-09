133f27916c2ad0c3153dd5f5961dbfd0b8b92a5a
Michael Kevin <michaelkmb@hotmail.com>
2023-03-09 10:38:34 -0300

    example code video_09;

---
+++ aula09.sh
@@ -0,0 +1,19 @@
+echo 'Inserindo ao arquivo'
+ls -laF  > testFolder/cat_aula09.txt
+cat testFolder/cat_aula09.txt
+
+echo '------------'
+echo 'Acrescentando ao fim arquivo'
+date >> testFolder/cat_aula09.txt
+cat testFolder/cat_aula09.txt
+
+echo '------------'
+echo 'Contando palavras no arquivo'
+wc < testFolder/cat_aula09.txt
+
+echo '------------'
+echo 'Redirecionamento inline'
+wc << EOF
+Teste de redirecionamento inline
+Curso de Shell Scripting
+EOF
